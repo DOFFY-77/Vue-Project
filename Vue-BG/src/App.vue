e2f1c78a5cebdf17163434b8ba2b2417f6a06f28
DOFFY-77 <ledbenh.dofy@proton.me>
2024-11-24 03:16:07 +0100

    creating navbar , hero and home cards sections

---
+++ Vue-BG/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import NavBar from "@/components/NavBar.vue";
+  import Hero from "@/components/Hero.vue";
+  import HomeCard from "@/components/HomeCard.vue";
+</script>
+
+<template>
+  <NavBar/>
+  <Hero title="Test title props"/>
+  <HomeCard/>
+</template>
